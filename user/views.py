635fc5e076fec6e062d80e9115612dfc618b521a
create <createcheng@yeah.net>
2016-11-22 23:08:56 +0800

    修改views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def register(request):
+    pass
+
+
+def settings(request):
+    pass
