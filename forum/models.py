c235ebd7e9472f0ea95293d252dc6eff9c638a1d
create <createcheng@yeah.net>
2016-11-23 23:06:12 +0800

    修改models。

--- forum/models.py
+++ forum/models.py
@@ -10,8 +10,19 @@ class Plate(models.Model):
 
 
 class Post(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='创建者')
     title = models.CharField(max_length=128, verbose_name='帖子标题')
     content = models.TextField(verbose_name='帖子内容')
     user = models.ForeignKey(User, verbose_name='创建者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间 ')
     plate = models.ForeignKey(Plate, related_name='posts')
+
+    class Meta:
+        verbose_name = '帖子'
+        verbose_name_plural = '帖子'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
 
     def __str__(self):
         return self.username
@@ -11,3 +12,18 @@ class User(AbstractUser):
     def get_absolute_url(self):
         pass
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profiles')
+    nickname = models.CharField(max_length=32, null=True, unique=True)
+    avatar = models.ImageField(upload_to='', blank=True, verbose_name='头像')
+    joined_time = models.DateTimeField(auto_now_add=True, verbose_name='加入时间')
+    joined_ip = models.GenericIPAddressField()
+    last_ip = models.GenericIPAddressField(null=True, blank=True)
+
+    def __str__(self):
+        return '{}的配置。'.format(self.user.username)
+
+    class Meta:
+        verbose_name = '用户配置'
+        verbose_name_plural = verbose_name
