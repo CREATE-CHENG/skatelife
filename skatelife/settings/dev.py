671092281fc9c06cf17b455308eea510916339e6
create <createcheng@yeah.net>
2016-11-24 22:47:04 +0800

    修改settings。

--- skatelife/settings/dev.py
+++ skatelife/settings/dev.py
@@ -1,3 +1,22 @@
 from .base import *
 
 SECRET_KEY = 'dev'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
