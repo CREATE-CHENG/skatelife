a15658ac1b9ba04cbba3ea665bd488ef07d7beef
create <createcheng@yeah.net>
2017-12-04 14:46:33 +0800

    用户回复通知功能雏形完成

--- comments/handlers.py
+++ comments/handlers.py
@@ -5,9 +5,13 @@ from .models import Comments
 
 
 def comment_handler(sender, instance, created, **kwargs):
-    if instance.creator != instance.post.creator:
+    if instance.reply_to:
+        notify.send(instance.creator, recipient=instance.reply_to.creator,
+                    verb='@了你。', action_object=instance,
+                    target=instance.post, description=instance.content)
+    else:
         notify.send(instance.creator, recipient=instance.post.creator,
-                    verb='发表了评论', action_object=instance,
+                    verb='发表了评论。', action_object=instance,
                     target=instance.post, description=instance.content)
 
 
