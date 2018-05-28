f0c235d760f7b44d472b5ca827911e4a4e441dae
create <createcheng@yeah.net>
2018-05-28 15:57:02 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # skatelife
 滑板综合社区网站
+基于Django及前端模板AdminLTE
+
+#demo
+    http://8.12.18.177
--- apps/forum/handlers.py
+++ apps/forum/handlers.py
@@ -4,12 +4,11 @@ from notifications.signals import notify
 from .models import Post
 
 
-# 测试发帖通知，用户关注功能完成后完善此通知
 def post_handler(sender, instance, created, **kwargs):
 
     notify.send(instance.creator, recipient=instance.creator.followed_by.all(),
                 verb='发表了帖子', action_object=instance,
-                target=instance, description=instance.content, type='follow')
+                target=instance, description=instance.content,)
 
 
 post_save.connect(post_handler, sender=Post)
